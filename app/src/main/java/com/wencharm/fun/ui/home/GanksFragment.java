a057b15cf71e769ba7de7d17bfcdebb7e63ffcb6
wencharmwang <wangwenqiang@p1.com>
2016-11-08 12:02:35 +0800

    nothing

--- app/src/main/java/com/wencharm/fun/ui/home/GanksFragment.java
+++ app/src/main/java/com/wencharm/fun/ui/home/GanksFragment.java
@@ -2,9 +2,11 @@ package com.wencharm.fun.ui.home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Fade;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,7 @@ import com.wencharm.fun.ui.BaseActivity;
 import com.wencharm.fun.ui.BaseFragment;
 import com.wencharm.fun.ui.RecyclerAdapter;
 import com.wencharm.fun.ui.profile.ProfileFragment;
+import com.wencharm.fun.ui.profile.ProfileTransiton;
 
 import java.util.List;
 
@@ -88,7 +91,7 @@ public class GanksFragment extends BaseFragment implements GanksContract.IView {
 
 		@Override
 		public void onBindViewHolder(GankHolder holder, int position) {
-			holder.bind(activity(), getItem(position));
+			holder.bind(activity(), getItem(position), position);
 		}
 
 	}
@@ -106,15 +109,26 @@ public class GanksFragment extends BaseFragment implements GanksContract.IView {
 			ButterKnife.bind(this, itemView);
 		}
 
-		void bind(BaseActivity activity, Gank.GankInfo gankInfo) {
+		void bind(BaseActivity activity, Gank.GankInfo gankInfo, int position) {
 			App.image.load(image, gankInfo.url);
 			provider.setText(gankInfo.who);
+			ViewCompat.setTransitionName(image, "image" + position);
 			card.setOnClickListener(v -> {
 				Bundle bundle = new Bundle();
 				bundle.putString(ProfileFragment.URL, gankInfo.url);
 				ProfileFragment fragment = new ProfileFragment();
 				fragment.setArguments(bundle);
-				activity.getSupportFragmentManager().beginTransaction().replace(R.id.content, fragment).addToBackStack(TAG).commit();
+				fragment.setSharedElementEnterTransition(new ProfileTransiton());
+				fragment.setSharedElementReturnTransition(new ProfileTransiton());
+				BaseFragment current = (BaseFragment) activity.getSupportFragmentManager().findFragmentById(R.id.content);
+				current.setEnterTransition(new Fade());
+				current.setExitTransition(new Fade());
+				activity.getSupportFragmentManager()
+						.beginTransaction()
+						.replace(R.id.content, fragment)
+						.addToBackStack(TAG)
+						.addSharedElement(image, "image")
+						.commit();
 			});
 		}
 
--- app/src/main/java/com/wencharm/fun/ui/profile/ProfileTransiton.java
+++ app/src/main/java/com/wencharm/fun/ui/profile/ProfileTransiton.java
@@ -1,8 +1,34 @@
 package com.wencharm.fun.ui.profile;
 
+import android.content.Context;
+import android.transition.ChangeBounds;
+import android.transition.ChangeImageTransform;
+import android.transition.ChangeTransform;
+import android.transition.TransitionSet;
+import android.util.AttributeSet;
+
 /**
  * Created by Wencharm on 07/11/2016.
  */
 
-public class ProfileTransiton {
+public class ProfileTransiton extends TransitionSet {
+
+	public ProfileTransiton() {
+		init();
+	}
+
+	/**
+	 * This constructor allows us to use this transition in XML
+	 */
+	public ProfileTransiton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	private void init() {
+		setOrdering(ORDERING_TOGETHER);
+		addTransition(new ChangeBounds()).
+				addTransition(new ChangeTransform()).
+				addTransition(new ChangeImageTransform());
+	}
 }
